1f988b2ca39073568c6c226fbe42a25d99690122
chefblogger <70014924+chefblogger@users.noreply.github.com>
2021-08-23 13:36:13 +0200

    joystick links und rechts scene 1

---
+++ Game/Assets/joystick_setting.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class joystick_setting : MonoBehaviour
+{
+
+
+    public GameObject JoystickLinks;
+    public GameObject JoystickRechts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        JoystickLinks.SetActive(true);
+        JoystickRechts.SetActive(false);
+    }
+
+ 
+}
